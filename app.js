1f102e1fc713411db7490b6b951188acde6905fc
Christopher Weinreich <weinreichchris@gmail.com>
2021-08-14 19:39:25 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Chris, Welcome to AWS!'));
+app.get('/', (req, res) => res.send('Hello Chris, Welcome to AWS and the beginning of your machine learning in production journey!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
